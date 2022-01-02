980df55c72fe81e9c55051db7741e7d712759642
eden.ju <eden.ju@shoplworks.com>
2022-01-02 14:28:44 +0900

    style: 테마 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Global, ThemeProvider } from "@emotion/react";
 import App from "./App";
+import reset from "./styles/reset";
+import theme from "./styles/theme";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Global styles={reset} />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    makerBlack: "#333",
+    makerWhite: "#fff",
+    makerLightGrey: "#e8e7e6",
+    makerLightPink: "#ffeae8",
+    makerGrey: "#626262",
+    makerGreen: "#385461",
+    makerPink: "pink",
+    makerShadow: "rgba(217,217,217,1)",
+    makerColorful:
+      "linear-gradient(166deg, rgba(237, 193, 211, 1) 0%,rgba(212, 149, 161, 1) 60%, rgba(162, 63, 63, 1) 100%, rgba(127, 188, 242, 1) 100%)",
+    makerWheat: "wheat",
+  },
+  size: {
+    mq: "62rem",
+  },
+};
+
+export default theme;
