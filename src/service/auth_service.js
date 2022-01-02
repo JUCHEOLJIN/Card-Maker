6c2d4968ddcaee41bb4ef2da097bc5f88856a87a
eden.ju <eden.ju@shoplworks.com>
2022-01-02 15:38:08 +0900

    feat: 파이어베이스 추가

---
+++ src/service/auth_service.js
@@ -0,0 +1,42 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+
+class AuthService {
+  constructor() {
+    this.firebaseAuth = getAuth();
+    this.googleProvider = new GoogleAuthProvider();
+    this.githubProvider = new GithubAuthProvider();
+  }
+
+  login(providerName) {
+    const authProvieder = this.getProvider(providerName);
+    return signInWithPopup(this.firebaseAuth, authProvieder);
+  }
+
+  logout() {
+    this.firebaseAuth.signOut();
+  }
+
+  onAuthChange(onUserChanged) {
+    this.firebaseAuth.onAuthStateChanged((user) => {
+      onUserChanged(user);
+    });
+  }
+
+  getProvider(providerName) {
+    switch (providerName) {
+      case "Google":
+        return this.googleProvider;
+      case "Github":
+        return this.githubProvider;
+      default:
+        throw new Error(`not supported provider : ${providerName}`);
+    }
+  }
+}
+
+export default AuthService;
---
+++ src/service/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_DB_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
