984c74568c18d81e191cd0a431d0b8bbd5ac485d
eden.ju <eden.j0303@gmail.com>
2022-01-11 15:35:20 +0900

    fix: cloudniary 계정 변경

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -2,9 +2,9 @@ class ImageUploader {
   async upload(file) {
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "r5lcdrhn");
+    data.append("upload_preset", "lfdowob2");
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/eden-card-maker/image/upload",
+      "https://api.cloudinary.com/v1_1/jay-card-maker/image/upload",
       {
         method: "POST",
         body: data,
