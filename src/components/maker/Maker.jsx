77c43c90842803892ea6d4a9f35322798d7dfcf7
eden.ju <public.jin03@gmail.com>
2022-01-12 23:05:53 +0900

    feat: 리얼타임데이터베이스 싱크타입 연결

--- src/components/maker/Maker.jsx
+++ src/components/maker/Maker.jsx
@@ -19,6 +19,16 @@ const Maker = ({ authService, FileInput, cardRepository }) => {
   };
 
   useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const stopSync = cardRepository.syncCards(userId, (cards) =>
+      setCards(cards)
+    );
+    return () => stopSync();
+  }, [userId]);
+
+  useEffect(() => {
     authService.onAuthChange((user) => {
       if (user) {
         setUserId(user.uid);
--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -1,8 +1,16 @@
-import { getDatabase, ref, remove, set } from "firebase/database";
+import { getDatabase, ref, onValue, remove, set } from "firebase/database";
 
 class CardRepository {
   constructor(app) {
     this.db = getDatabase(app);
+  }
+
+  syncCards(userId, onUpdate) {
+    onValue(ref(this.db, `/${userId}/cards`), (snapshot) => {
+      const value = snapshot.val();
+      value && onUpdate(value);
+    });
+    return () => ref.off();
   }
 
   saveCard(userId, card) {
